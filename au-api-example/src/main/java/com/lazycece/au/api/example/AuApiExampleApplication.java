bd45d4c15de18986f861d682d583d0ca26f4d1a8
lazycece <lazycece@gmail.com>
2020-01-26 14:59:47 +0800

    au api example

---
+++ au-api-example/src/main/java/com/lazycece/au/api/example/AuApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.au.api.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuApiExampleApplication.class, args);
+    }
+
+}
---
+++ au-api-example/src/test/java/com/lazycece/au/api/example/AuApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazycece.au.api.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuApiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
