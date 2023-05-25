b8d4d34a765b99d1d763ef3c1075ca15de868b2c
lazycece <lazycece@gmail.com>
2023-05-25 23:27:41 +0800

    upgrade java 17

--- au-api-example/src/main/java/com/lazycece/au/api/example/entity/req/LoginReq.java
+++ au-api-example/src/main/java/com/lazycece/au/api/example/entity/req/LoginReq.java
@@ -3,7 +3,7 @@ package com.lazycece.au.api.example.entity.req;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 /**
  * @author lazycece
